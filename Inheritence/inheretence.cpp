9cb44dd1e673b2cc54cb89399b89d1525ee3726f
Zehab-bid <zehabidha152gmail.com>
2025-05-27 16:26:54 +0700

    proges 3

--- Inheritence/inheretence.cpp
+++ Inheritence/inheretence.cpp
@@ -28,4 +28,12 @@ public:
     pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
         cout << "pelajar dibuat\n" << endl;
     }
-}
+
+    ~pelajar() {
+        cout << "pelajar dihapus\n" << endl;
+    }
+
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+    }
+};
