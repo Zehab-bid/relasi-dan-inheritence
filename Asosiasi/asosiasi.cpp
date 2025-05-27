8732d2de8179ad5274e93f5767ab2c650ccd7e1a
Zehab-bid <zehabidha152gmail.com>
2025-05-27 16:21:31 +0700

    proges 9

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -77,4 +77,11 @@ int main() {
     varPasien1->cetakDokter();
     varPasien2->cetakDokter();
 
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
+
+    return 0;
+
 }
