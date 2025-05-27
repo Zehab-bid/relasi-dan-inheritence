b556cab6652ed1b2522301dbea0f69b31df75048
Zehab-bid <zehabidha152gmail.com>
2025-05-27 16:24:02 +0700

    proges 10

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -4,84 +4,84 @@ using namespace std;
 
 class dokter;
 
-class pasien{
-    public:
+class pasien { // asosiasi
+public:
     string nama;
     vector<dokter*> daftar_dokter;
-    pasien(string pNama): nama(pNama) {
+
+    pasien(string pNama) : nama(pNama) {
         cout << "Pasien \"" << nama << "\" ada\n";
     }
+
     ~pasien() {
-        cout << "Pasien \"" << nama
-        << "\" tidak ada\n";
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
     }
-    void tambahanDokter(dokter*);
+
+    void tambahDokter(dokter*);
     void cetakDokter();
 };
 
 class dokter {
-    public:
-        string nama;
-        vector<pasien*> daftar_pasien;
+public:
+    string nama;
+    vector<pasien*> daftar_pasien;
 
-        dokter(string pNama): nama(pNama) {
-            cout << "dokter \"" << nama << "\" ada\n";
+    dokter(string pNama) : nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahPasien(pasien*);
+    void cetakPasien();
+
+    void pasien::tambahDokter(dokter* pDokter) {
+        daftar_dokter.push_back(pDokter);
+    }
+
+    void dokter::tambahPasien(pasien* pPasien) {
+        daftar_pasien.push_back(pPasien);
+        pPasien->tambahDokter(this);
+    }
+
+    void dokter::cetakPasien() {
+        cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+        for (auto& a : daftar_pasien) {
+            cout << a->nama << "\n";
         }
-        ~dokter() {
-            cout << "Dokter \"" << nama
-            << "\" tidak ada\n";
+        cout << endl;
+    }
+
+    void pasien::cetakDokter() {
+        cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+        for (auto& a : daftar_dokter) { // auto digunakan dalam perulangan for untuk secara otomatis menentukan tipe data dari elemen yang diiterasi
+            cout << a->nama << "\n";
         }
-        void tambahanPasien(pasien*);
-        void cetakPasien();
-    
+        cout << endl;
+    }
+
+    int main() {
+        dokter* varDokter1 = new dokter("dr.Budi");
+        dokter* varDokter2 = new dokter("dr.Tono");
+        pasien* varPasien1 = new pasien("Andi");
+        pasien* varPasien2 = new pasien("Rina");
+
+        varDokter1->tambahPasien(varPasien1);
+        varDokter1->tambahPasien(varPasien2);
+        varDokter2->tambahPasien(varPasien1);
+
+        varDokter1->cetakPasien();
+        varDokter2->cetakPasien();
+        varPasien1->cetakDokter();
+        varPasien2->cetakDokter();
+
+        delete varPasien1;
+        delete varPasien2;
+        delete varDokter1;
+        delete varDokter2;
+
+        return 0;
+    }
 };
-
-void pasien::tambahanDokter(dokter* pDokter){
-    daftar_dokter.push_back(pDokter);
-}
-
-
-void pasien::cetakDokter() {
-    cout << "Daftar dokter yang menangani pasien \""
-    << this->nama << "\":\n";
-    // aouto digunakan dalam perulangan
-    for (auto& a : daftar_dokter) {
-        cout << a->nama << "\n";
-    }
-    cout << endl;
-}
-void dokter:: tambahanPasien(pasien* pPasien) {
-    daftar_pasien.push_back(pPasien);
-    pPasien->tambahanDokter(this);
-}
-void dokter::cetakPasien() {
-    cout << "Daftar pasien dari dokter \"" << this->nama << "\":\n";
-    for (auto& a : daftar_pasien) {
-        cout << a->nama << "\n";
-    }
-    cout << endl;
-}
-
-int main() {
-    dokter* varDokter1 = new dokter("dr.Budi");
-    dokter* varDokter2 = new dokter("dr.Tono");
-    dokter* varPasien1 = new dokter("Andi");
-    dokter* varPasien2 = new dokter("Lia");
-
-    varPasien1->tambahanPasien(varDokter1);
-    varPasien1->tambahanPasien(varDokter2);
-    varPasien2->tambahanPasien(varDokter2);
-
-    varDokter1->cetakPasien();
-    varDokter2->cetakPasien();
-    varPasien1->cetakDokter();
-    varPasien2->cetakDokter();
-
-    delete varPasien1;
-    delete varPasien2;
-    delete varDokter1;
-    delete varDokter2;
-
-    return 0;
-
-}
