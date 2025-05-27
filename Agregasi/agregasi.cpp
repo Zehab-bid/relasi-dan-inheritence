320ab1566ab207c91398a5ee7bc327bacca50a50
Zehab-bid <zehabidha152gmail.com>
2025-05-27 15:20:51 +0700

    proges 5

--- Agregasi/agregasi.cpp
+++ Agregasi/agregasi.cpp
@@ -5,3 +5,15 @@ using namespace std;
 #include "Anak.H"
 #include "ibu.H"
 
+int main() {
+    ibu* varIbu = new ibu("dini");
+    ibu* varIbu2 = new ibu("novi");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
+}
